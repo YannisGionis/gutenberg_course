1cd3330a512228afcea3fd54c8cfae69d1be35fb
YannisGionis <gionjo@gmnail.com>
2025-10-03 13:11:59 +0300

    plugin metadata

--- plugin.php
+++ plugin.php
@@ -1,13 +1,11 @@
 <?php
 /**
  * Plugin Name:       Team Members
- * Description:       A team members grid.
+ * Description:       Gutenberg course plugin
  * Requires at least: 5.7
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            Ali Alaa
- * License:           GPL-2.0-or-later
- * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Author:            Yannis Gkionis
  * Text Domain:       team-members
  *
  * @package           blocks-course
